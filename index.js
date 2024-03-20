96a93eb6d4cf6a8c53c265fe2a16980174d669e0
Boombox2610 <137889383+Boombox2610@users.noreply.github.com>
2024-03-20 23:37:51 +0530

    Mian files Commit #1
    indexed base file

---
+++ index.js
@@ -0,0 +1,7 @@
+// document.querySelector('.card').addEventListener('click', function() {
+//     this.classList.toggle('expanded');
+//     Array.from(this.querySelectorAll('.section')).forEach(function(section) {
+//         section.classList.toggle('hidden');
+//     });
+// });
+
